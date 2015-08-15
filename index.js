c1197b97ea524ac12535b3ace71348a65a024de5
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-15 05:48:51 -0400

    refactor to non-constructor function

--- index.js
+++ index.js
@@ -7,15 +7,9 @@
 
 'use strict';
 
-var extend = require('extend-shallow');
 var expand = require('expand-object');
 
-function Expander(argv) {
-  this.argv = argv || {};
-}
-
-Expander.prototype.splitOpts = function(argv) {
-  argv = argv || this.argv;
+function expandArgs(argv) {
   var res = {};
 
   for (var key in argv) {
@@ -32,63 +26,34 @@ Expander.prototype.splitOpts = function(argv) {
       continue;
     }
 
-    if (Array.isArray(orig) && key !== '_') {
-      res[key] = orig.map(function (ele) {
-        return expand(ele, {toBoolean: true});
-      });
+    if (Array.isArray(orig)) {
+      res[key] = expandEach(orig);
       continue;
     }
 
     if (typeof orig === 'object' && key !== '_') {
-      res[key] = this.splitOpts(orig);
+      res[key] = expandArgs(orig);
       continue;
     }
 
-    if (/\W/.test(val)) {
-      res[key] = expand(val, {toBoolean: true});
+    if (typeof orig === 'string' && /\W/.test(orig)) {
+      res[key] = expand(orig, {toBoolean: true});
       continue;
     }
-
     res[key] = orig;
   }
-
-  this.argv = res;
   return res;
-};
+}
 
-Expander.prototype.splitArgs = function(argv) {
-  argv = argv || this.argv;
-  var arr = (argv._ || []).slice(0);
-  var len = arr.length, i = -1;
-  var num = -1;
-  while (++i < len) {
-    var arg = arr[i];
-    var val = arg.split(':');
-    if (val.length > 1) {
-      argv[val[0]] = val[1];
-      argv._.splice(i - (++num), 1);
-    }
-  }
-  return argv;
-};
-
-Expander.prototype.expand = function(argv) {
-  this.splitArgs(argv);
-  this.splitOpts(argv);
-  return this.argv;
-};
+function expandEach(arr) {
+  return arr.map(function (ele) {
+    if (!/\W/.test(ele)) return ele;
+    return expand(ele, {toBoolean: true});
+  });
+}
 
 /**
- * Expose an instance of `Expander`
+ * Expose an instance of `expandArgs` plugin
  */
 
-module.exports = function (argv) {
-  var expander = new Expander();
-  return expander.expand(argv);
-};
-
-/**
- * Expose an the `Expander` constructor
- */
-
-module.exports.Expander = Expander;
+module.exports = expandArgs;
