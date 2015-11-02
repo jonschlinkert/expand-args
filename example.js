4b9863c44a43420070025660c80de0ca8623a719
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-02 18:55:55 -0500

    lint

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 var expandArgs = require('./');
 var minimist = require('minimist');
-var argv = minimist(['--set=a:b,c:d']);
-console.log(argv);
-//=> { _: [], set: 'a:b,c:d' }
+// var argv = minimist(['--set=a:b,c:d']);
+// console.log(argv);
+// //=> { _: [], set: 'a:b,c:d' }
+// console.log(expandArgs(argv));
+// //=> { _: [], set: [{ a: 'b'}, {c: 'd'}] }
+
+var argv = minimist(process.argv.slice(2));
 console.log(expandArgs(argv));
-//=> { _: [], set: [{ a: 'b'}, {c: 'd'}] }
